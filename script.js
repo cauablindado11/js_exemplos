50fac1e9258072ab8d32de0e459893dff376f41a
Cauã Lucas <126674336+cauablindado11@users.noreply.github.com>
2023-05-24 14:51:46 -0300

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+        // PRIMEIRA AULA
+
+// var valor = confirm("Vamos juntos aprender a programar em JS?")
+// if(valor == true){
+//     alert("Parábens, vamos lá!!!");
+// } else {
+//     alert("Opção errada, aperte F5 para recarregar a página.");
+// }
+
+
+        // SEGUNDA AULA
+
+
+//  var email = prompt("Digite seu e-mail")
+
+//  alert("O e-mail " + email + " foi coletado com sucesso.");
+
+
+// if ( email == null || email == "" ){
+//     alert("Não conseguimos coletar seu email.")
+// }
+// else{
+//     alert("Coletamos seu email com sucesso.")
+// }
+
+
+        // TERCEIRA AULA
+
+
+var prova = parseFloat (prompt (" Qual a nota da prova? "));
+var trabalho = parseFloat (prompt (" Qual a nota do trabalho? "));
+
+var notaFinal = prova + trabalho;
+
+alert(notaFinal);
